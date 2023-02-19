3f3d7a2fe3b56e308120a812d801822d82ab9558
Tanuki3017` <zds8702@rit.edu>
2023-02-19 14:34:27 -0500

    Added server code

--- src/server.js
+++ src/server.js
@@ -1,9 +1,72 @@
 const http = require('http');
-const url = require('url'); 
+const url = require('url');
 const query = require('querystring');
 const htmlHandler = require('./htmlResponses.js');
 const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+  GET: {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCSS,
+    '/getUsers': jsonHandler.getUsers,
+    '/notFound': jsonHandler.notFound,
+    notFound: jsonHandler.notFound,
+  },
+
+  HEAD: {
+    '/getUsers': jsonHandler.getUsersMeta,
+    '/notFound': jsonHandler.notFoundMeta,
+    notFound: jsonHandler.notFoundMeta,
+  },
+};
+
+const parseBody = (request, response, handler) => {
+  const body = [];
+
+  request.on('error', (err) => {
+    console.dir(err);
+    response.statusCode = 400;
+    response.end();
+  });
+
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    const bodyParams = query.parse(bodyString);
+
+    handler(request, response, bodyParams);
+  });
+};
+
+const handlePost = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/addUser') {
+    parseBody(request, response, jsonHandler.addUser);
+  }
+};
+
+const handleGETHEAD = (request, response, parsedUrl) => {
+  if (!urlStruct[request.method]) {
+    urlStruct.HEAD.notFound(request, response);
+  } else if (urlStruct[request.method][parsedUrl.pathname]) {
+    urlStruct[request.method][parsedUrl.pathname](request, response);
+  } else {
+    urlStruct[request.method].notFound(request, response);
+  }
+};
+
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+
+  if (request.method === 'POST') {
+    handlePost(request, response, parsedUrl);
+  } else {
+    handleGETHEAD(request, response, parsedUrl);
+  }
+};
 
 http.createServer(onRequest).listen(port);
